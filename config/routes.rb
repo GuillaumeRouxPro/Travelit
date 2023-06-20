4e1a10c78f8b3884158f66c472a67bffd2b7068d
Guillaume Roux <guillaume500@hotmail.fr>
2023-06-20 20:05:50 +0200

    models and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :tours do
+    resources :bookings, only: [:new, :create]
+    resources :reviews, only: [:index, :create]
+  end
+
+  resources :bookings, only: [:index, :show, :edit, :update, :destroy] do
+    member do
+      patch 'accept'
+      patch 'refuse'
+    end
+    resources :reviews, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+#USERS SEEDS
+puts "Destroying users..."
+User.destroy_all
+puts "Creating user..."
+User.create!(
+  email: "michel@hotmail.fr",
+  encrypted_password: "123456"
+  first_name: "Michel"
+  last_name: "Aulas"
+)
+User.create!(
+  email: "guillaume@hotmail.fr",
+  encrypted_password: "123456"
+  first_name: "Guillaume"
+  last_name: "Roux"
+)
+puts "Finished create users!"
+
+#HOBBIES SEEDS
+puts "Destroying Hobbies..."
+Hobby.destroy_all
+puts "Creating Hobbies..."
+Hobby.create!(
+  name: "Football",
+)
+Hobby.create!(
+  name: "Tennis",
+)
+puts "Finished create Hobbies!"
+
+
+#REVIEWS SEEDS
+puts "Destroying Reviews..."
+Review.destroy_all
+puts "Creating Reviews..."
+Review.create!(
+  comment: "Super tour !",
+  rating: 5,
+)
+Review.create!(
+  comment: "Nice tour !",
+  rating: 3,
+)
+puts "Finished create Reviews!"
