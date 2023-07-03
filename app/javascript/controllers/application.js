ac056233866fe7a2c2f38564c7639ca4a4eaf7dd
Guillaume Roux <guillaume500@hotmail.fr>
2023-07-03 17:36:19 +0200

    home form upgrade, starting the show page of a tour, and starting the booking process

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,13 @@
 import { Application } from "@hotwired/stimulus"
 
+//= require select2
+$(document).ready(function() {
+  $('.select2').select2();
+});
+
+
+
+
 const application = Application.start()
 
 // Configure Stimulus development experience
--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,4 +1,6 @@
 class Hobby < ApplicationRecord
-  has_many :user_hobbies, dependent: :destroy
   has_many :tours, through: :user_hobbies
+
+  has_many :user_hobbies
+  has_many :users, through: :user_hobbies
 end
