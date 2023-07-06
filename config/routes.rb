95ec631fa68d261beb0557df6052e2fc29f0e2df
Eyosias Woldemichael <eyosias16@gmail.com>
2023-07-06 16:03:35 +0200

    Ensuired the seach and the filtering algorithm

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/search', to: 'home#search'
+  get 'home/result', to: 'home#result',  as: 'home_result'
   devise_for :users
   root to: "tours#index"
 
   get '/tours/new', to: 'tours#new', as: 'new_tour'
 
-  resources :tours
+  resources :tours do
+    collection do
+      get :top_match
+    end
+  end
   resources :hobbies, only: [:index, :new, :create]
 
-  #resources :tours do
+    #resources :tours do
     #resources :bookings, only: [:new, :create]
     #resources :reviews, only: [:index, :create]
   #end
